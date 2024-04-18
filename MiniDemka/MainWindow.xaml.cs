90ffec7a7f9ec47fab873967c75ee1ab0aa532bc
AS US <amik54321@mail.ru>
2024-04-18 15:46:52 +0400

    цу

--- MiniDemka/MainWindow.xaml.cs
+++ MiniDemka/MainWindow.xaml.cs
@@ -37,104 +37,9 @@ namespace MiniDemka
         public void Output()
         {
             events = app.Events.ToList();
-            cities = app.City.ToList();
-            int? cId;
-
-            for (int i = 0; i < events.Count; i++) 
-            {
-                cId = events[i].CityID;
-                events[i].CityName = cities.FirstOrDefault(xe => xe.ID == cId).Name;
-            }
-            eventsSort = app.Events.ToList();
-            eventsSearch = app.Events.ToList();
             evList.ItemsSource = events;
         }
 
-        private void buttonOrderBy_Click(object sender, RoutedEventArgs e)
-        {
-            srt = 0;
-            Func();
-        }
-
-        private void buttonOrderByDesc_Click(object sender, RoutedEventArgs e)
-        {
-            srt = 1;
-            Func();
-            
-        }
-
-        private void searchTxb_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            Func();
-        }
-
-        public void Func()
-        {
-            if (srtClear == 1)
-            {
-                Output();
-                searchTxb.Text = "";
-            }
-            else
-            {
-                var eventsWithAll = events;
-                if (searchTxb.Text != "")
-                {
-                    eventsWithAll = eventsWithAll.Where(x => x.Event.Contains(searchTxb.Text)).ToList();
-                    evList.ItemsSource = eventsWithAll;
-                }
-                else
-                {
-                    evList.ItemsSource = events;
-                    switch (srt)
-                    {
-                        case 0:
-                            events = events.OrderBy(xe => xe.Date).ToList();
-                            evList.ItemsSource = events;
-                            break;
-                        case 1:
-                            events = events.OrderByDescending(xe => xe.Date).ToList();
-                            evList.ItemsSource = events;
-                            break;
-                    }
-                }
-                switch (srt)
-                {
-                    case 0:
-                        eventsWithAll = eventsWithAll.OrderBy(xe => xe.Date).ToList();
-                        evList.ItemsSource = eventsWithAll;
-                        break;
-                    case 1:
-                        eventsWithAll = eventsWithAll.OrderByDescending(xe => xe.Date).ToList();
-                        evList.ItemsSource = eventsWithAll;
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            
-        }
-
-        private void cleadrSrtFlt_Click(object sender, RoutedEventArgs e)
-        {
-            srtClear = 1;
-            Func();
-            srtClear = 0;
-        }
-
-        private void redButton_Click(object sender, RoutedEventArgs e)
-        {
-            Button b = sender as Button;
-            int t = (int)b.Tag;
-            Add a = new Add("red", t);
-            a.ShowDialog();
-            evList.ItemsSource = null;
-            Output();
-            MainWindow m = new MainWindow();
-            m.Show();
-            this.Close();
-        }
 
         private void deleteButton_Click(object sender, RoutedEventArgs e)
         {
@@ -158,7 +63,7 @@ namespace MiniDemka
 
         private void addButton_Click(object sender, RoutedEventArgs e)
         {
-            Add a = new Add("add", 0);
+            Add a = new Add();
             a.ShowDialog();
             evList.ItemsSource = null;
             Output();
